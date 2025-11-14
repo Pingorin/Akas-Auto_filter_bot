7c9693043e2936d31b5bec83a238d3e4600710c6
Pingorin <125970228+Pingorin@users.noreply.github.com>
2025-11-15 01:42:48 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,24 +1,16 @@
 # info.py
 import os
 
-# Telegram Bot Token from @BotFather
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "YOUR_BOT_TOKEN_HERE")
-
-# API Keys from my.telegram.org
-API_ID = int(os.environ.get("API_ID", 123456))
-API_HASH = os.environ.get("API_HASH", "YOUR_API_HASH_HERE")
-
-# Bot Owner ID (https://t.me/userinfobot से अपनी ID प्राप्त करें)
-OWNER_ID = int(os.environ.get("OWNER_ID", 123456789))
-
-# MongoDB (Motor)
-MONGO_DB_URI = os.environ.get("MONGO_DB_URI", "YOUR_MONGO_DB_URI_HERE")
-
-# Bot settings
-BOT_USERNAME = os.environ.get("BOT_USERNAME", "YourBotUsername") # (Without @)
-Norm_pic = os.environ.get("Norm_pic", "https://graph.org/file/4d61886e61dfa37a25945.jpg")
-
-# Channel and Owner
-UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "https://t.me/+UWok0DWTWTpkYTA1")
-MAIN_CHANNEL = os.environ.get("MAIN_CHANNEL", "https://t.me/+wVUitwQcus82OGVl")
+# --- यह 4 वेरिएबल्स बहुत ज़रूरी हैं ---
+BOT_USERNAME = os.environ.get("BOT_USERNAME", "Aks_Auto_F_Sub_bot") # (Without @)
+UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "https://t.me/+wVUitwQcus82OGVl")
+MAIN_CHANNEL = os.environ.get("MAIN_CHANNEL", "https://t.me/+UWok0DWTWTpkYTA1")
 OWNER_USERNAME = os.environ.get("OWNER_USERNAME", "ramSitaam") # (Without @)
+
+# --- यह भी ज़रूरी हैं ---
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8538484625:AAEMp2gncTCuIFR-VZcmVs_81vBHSNPPwO4")
+API_ID = int(os.environ.get("API_ID", 20638104))
+API_HASH = os.environ.get("API_HASH", "6c884690ca85d39a4c5ad7c15b194e42")
+OWNER_ID = int(os.environ.get("OWNER_ID", 7245547751))
+MONGO_DB_URI = os.environ.get("MONGO_DB_URI", "mongodb+srv://anu77:anu77@cluster0.8ohtzju.mongodb.net/")
+Norm_pic = os.environ.get("Norm_pic", "https://graph.org/file/4d61886e61dfa37a25945.jpg")
