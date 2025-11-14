236ebd16d164604a8f8ecb1edf01364089112757
Pingorin <125970228+Pingorin@users.noreply.github.com>
2025-11-14 22:22:12 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -4,8 +4,7 @@ from pyrogram import filters, Client
 from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
 from database.ia_filterdb import find_files
 
-@app.on_message(filters.private & filters.text & ~filters.command())
-
+@app.on_message(filters.private & filters.text & ~filters.command) # <-- यह सही लाइन है
 async def pm_filter_handler(client: Client, message: Message):
     """
     ऑटो-फिल्टर (सर्च) हैंडलर
