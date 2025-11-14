d90aed72c984d628e5afe52ee70964081866a88e
Pingorin <125970228+Pingorin@users.noreply.github.com>
2025-11-15 02:01:47 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,13 +1,13 @@
 # info.py
 import os
 
-# --- यह 4 वेरिएबल्स बहुत ज़रूरी हैं ---
-BOT_USERNAME = os.environ.get("BOT_USERNAME", "Aks_Auto_F_Sub_bot") # (Without @)
+# यह 4 वेरिएबल्स utils.py के लिए ज़रूरी हैं
+BOT_USERNAME = os.environ.get("BOT_USERNAME", "Aks_Auto_F_Sub_bot")
 UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "https://t.me/+wVUitwQcus82OGVl")
 MAIN_CHANNEL = os.environ.get("MAIN_CHANNEL", "https://t.me/+UWok0DWTWTpkYTA1")
-OWNER_USERNAME = os.environ.get("OWNER_USERNAME", "ramSitaam") # (Without @)
+OWNER_USERNAME = os.environ.get("OWNER_USERNAME", "ramSitaam") # <-- यह लाइन गायब है
 
-# --- यह भी ज़रूरी हैं ---
+# बाकी वेरिएबल्स
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "8538484625:AAEMp2gncTCuIFR-VZcmVs_81vBHSNPPwO4")
 API_ID = int(os.environ.get("API_ID", 20638104))
 API_HASH = os.environ.get("API_HASH", "6c884690ca85d39a4c5ad7c15b194e42")
