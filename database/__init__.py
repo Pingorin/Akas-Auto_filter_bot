b9b8c084b51e96dcaef65363be408c1813a5ac6d
Pingorin <125970228+Pingorin@users.noreply.github.com>
2025-11-14 22:21:03 +0530

    Update __init__.py

--- database/__init__.py
+++ database/__init__.py
@@ -1,7 +1,7 @@
 # database/__init__.py
 from motor.motor_asyncio import AsyncIOMotorClient
-from info import MONGO_DB_URI  # <-- यह सही तरीका है (Info.MONGO_DB_URI नहीं)
+from info import MONGO_DB_URI  # <-- यह सही लाइन है
 
 # Initialize Motor Client
 client = AsyncIOMotorClient(MONGO_DB_URI)
-db = client.telegram_bot_db  # आप चाहें तो 'telegram_bot_db' का नाम बदल सकते हैं
+db = client.telegram_bot_db
